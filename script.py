3a0107ac58127a3f634f45d11e58ec7e6e40f7b0
drunkrabbit1 <acer5742zg@mail.ru>
2020-03-14 23:00:04 +0300

    v1.2

--- script.py
+++ script.py
@@ -1,13 +1,12 @@
 import vk_api # Импорт библиотеке vk api
-import datetime # Импорт библиотеке datetime
-import time # Импорт библиотеке time
 from random import randint
 
+
+tkn='fb9de23c1ced0607334808e92e4e2de1b5fb2817b6cfff7a9e4f7436afaefcf65898f76f8b99a1659e9bf'
 while True: # Запускаем бесконечный цикл
-    vk = vk_api.VkApi(token="4f8ad799b1cfbbf23ad4829a97f8840c88a3c1cc6b37f85521c896845a65c5fdd247f73bf859ddf8a111a") # вот здесь нужно вписать ваш acces token. Чтобы получить его перейдите на этот сюда
-    delta = datetime.timedelta(hours=3, minutes=0) # разница от UTC. Можете вписать любое значение вместо «3» ставите ваш пояс.
+    vk = vk_api.VkApi(token=tkn) # вот здесь нужно вписать ваш acces token.
     prefix = open('base.txt')
     data = prefix.readlines()
     rand = randint(0, len(data) - 1)
     vk.method("status.set", {"text": data[rand]}) # указано содержимое статуса.
-    time.sleep(60) # анти-каптча. Погружает скрипт в «сон» на 30 секунд
+    time.sleep(120) # анти-каптча. Погружает скрипт в «сон» на 30 секунд
