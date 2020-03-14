0c5f080ef06a98d2df6cd96dd18a398db27ff234
drunkrabbit1 <acer5742zg@mail.ru>
2020-03-14 23:58:14 +0300

    v1.4

--- script.py
+++ script.py
@@ -4,11 +4,16 @@ import time
 from random import randint
 import json
 
+
 tkn='fb9de23c1ced0607334808e92e4e2de1b5fb2817b6cfff7a9e4f7436afaefcf65898f76f8b99a1659e9bf'
+base = open('base.json')
+
+# js = json.dumps('base.json')
+jsonData = json.loads(base.read())
+print(len(jsonData['prefix']) - 1)
 while True: # Запускаем бесконечный цикл
     vk = vk_api.VkApi(token=tkn) # вот здесь нужно вписать ваш acces token.
-    prefix = open('base.txt')
-    data = prefix.readlines()
-    rand = randint(0, len(data) - 1)
-    vk.method("status.set", {"text": data[rand]}) # указано содержимое статуса.
-    time.sleep(120) # анти-каптча. Погружает скрипт в «сон» на 30 секунд
+    rand = randint(0, len(jsonData['prefix']) - 1) # рандомное число
+    # указано содержимое статуса.
+    vk.method("status.set", {"text": jsonData['prefix'][rand]['text']})
+    time.sleep(120) # анти-каптча. Погружает скрипт в «сон» на 120 секунд
