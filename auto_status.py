b7668425181eb936207e57c9ae392324b8374b6b
drunkrabbit1 <acer5742zg@mail.ru>
2020-03-18 00:00:04 +0300

    v1.7

---
+++ auto_status.py
@@ -0,0 +1,37 @@
+import vk_api
+import time
+from random import randint
+import json
+
+"""
+Описание:
+1. Подключения объекта к пользлователю
+2. Json монипуляции
+  2.1 Подключения json_file
+  2.2 Аторизация пользователя
+  2.3 Методы
+
+"""
+
+
+class AutoStatus:
+
+    def __init__(self, token):
+        self.token = token
+        self.vk = vk_api.VkApi(token=token)
+
+    def json_file(self, name):
+        base = open(name)
+        self.json = json.loads(base.read())
+        return self
+
+    def user(self, name):
+        self.user_name = name
+        return self
+
+    def status_set_text(self):
+        rand = randint(0, len(self.json[self.user_name]) - 1) # рандомное число
+        self.vk.method("status.set",{"text": self.json[self.user_name][rand]['text']})
+
+    def anti_captcha(self, second):
+        time.sleep(second)
--- script.py
+++ script.py
@@ -1,16 +1,8 @@
+from auto_status import AutoStatus
 
-import vk_api # Импорт библиотеке vk api
-import time
-from random import randint
-import json
+tkn = 'fb9de23c1ced0607334808e92e4e2de1b5fb2817b6cfff7a9e4f7436afaefcf65898f76f8b99a1659e9bf'
+object1 = AutoStatus(token=tkn).json_file(name='base.json').user(name='fallen')
 
-tkn='fb9de23c1ced0607334808e92e4e2de1b5fb2817b6cfff7a9e4f7436afaefcf65898f76f8b99a1659e9bf'
-base = open('base.json')
-jsonData = json.loads(base.read())
-
-while True: # Запускаем бесконечный цикл
-    vk = vk_api.VkApi(token=tkn) # вот здесь нужно вписать ваш acces token.
-    rand = randint(0, len(jsonData['prefix']) - 1) # рандомное число
-    # указано содержимое статуса.
-    vk.method("status.set", {"text": jsonData['prefix'][rand]['text']})
-    time.sleep(120) # анти-каптча. Погружает скрипт в «сон» на 120 секунд
+while True:
+    object1.status_set_text()
+    object1.anti_captcha(240)
