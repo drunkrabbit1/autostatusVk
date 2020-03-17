e3919f53d6cd7d3f46489f74200b26944b57c21e
drunkrabbit1 <acer5742zg@mail.ru>
2020-03-18 01:41:01 +0300

    v1.8

--- script.py
+++ script.py
@@ -5,11 +5,7 @@ tkn1 = 'fb9de23c1ced0607334808e92e4e2de1b5fb2817b6cfff7a9e4f7436afaefcf65898f76f
 object1 = AutoStatus(token=tkn1).json_file(name='base.json').user('fallen')
 # object2 = AutoStatus(token=tkn2).json_file(name='base.json').user(name='None')
 get_sec = 0
+timing = 6
 while True:
-    try:
-        object1.status_set_text()
-        print('True')
-    except exceptions_captcha() as captcha:
-        print("Прошло {}".format(get_sec))
-        get_sec += 30
-        anti_captcha(6)
+    object1.status_set_text()
+    anti_captcha(6)
