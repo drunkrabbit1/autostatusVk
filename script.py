456e3e4abea9adaccf8cd7c3e7d8682c9329bb7c
drunkrabbit1 <acer5742zg@mail.ru>
2020-03-19 23:15:53 +0300

    v2.0

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ tkn1 = 'fb9de23c1ced0607334808e92e4e2de1b5fb2817b6cfff7a9e4f7436afaefcf65898f76f
 tkn2 = '2ecedf27e3822ab9b902375dd5611f057bce079907cca5576e761e1076205c54bf0a9b226476387a1cb16'
 
 object1 = AutoStatus(token=tkn1).json_file().user('fallen')
-object2 = AutoStatus(token=tkn1).json_file().user('none')
+object2 = AutoStatus(token=tkn2).json_file().user('none')
 
 while True:
     object1.status_set_text()
