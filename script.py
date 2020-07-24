dcb5142b941031a7eb1881889e60ff290c71b1bd
drunk_rabbit <32393133+drunkrabbit1@users.noreply.github.com>
2020-07-24 20:46:25 +0300

    Update script.py

--- script.py
+++ script.py
@@ -3,8 +3,8 @@ from auto_status import AutoStatus, anti_captcha, exceptions_captcha
 tkn1 = ''
 tkn2 = ''
 
-object1 = AutoStatus(token=tkn1).json_file().user('fallen')
-object2 = AutoStatus(token=tkn2).json_file().user('none')
+object1 = AutoStatus(token=tkn1).json_file().user('')
+object2 = AutoStatus(token=tkn2).json_file().user('')
 
 while True:
     object1.status_set_text()
